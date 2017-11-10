698dc2e63d04f919e34a4967adefcdc2b3b90fac
lucaslanziloti <lucas.lanziloti@vitaebrasil.com.br>
2017-11-10 11:20:29 -0200

    Adição da tela de usuário

---
+++ src/main/java/br/com/trocaJogos/controller/LoginMb.java
@@ -0,0 +1,72 @@
+package br.com.trocaJogos.controller;
+
+import br.com.trocaJogos.dao.UsuarioDao;
+import br.com.trocaJogos.model.Usuario;
+import br.com.trocaJogos.util.ViewUtil;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * @author lucas
+ */
+@SessionScoped
+@ManagedBean(name = "loginMb")
+public class LoginMb {
+
+    private UsuarioDao usuarioDao = new UsuarioDao();
+
+    private Usuario usuarioLogado = new Usuario();
+    private String email = "";
+    private String senha = "";
+
+    @PostConstruct
+    private void init() {
+    }
+
+    public void logar() {
+        Usuario usuario = usuarioDao.logar(email, senha);
+
+        if (usuario == null || usuario.getId() == null) {
+            ViewUtil.adicionarMensagemDeErro("Email e senha incorretos!");
+            sair();
+        } else {
+            usuarioLogado = usuario;
+
+            email = "";
+            senha = "";
+
+            ViewUtil.adicionarMensagemDeSucesso("Logado com Sucesso!");
+        }
+    }
+
+    public void sair() {
+        usuarioLogado = new Usuario();
+        email = "";
+        senha = "";
+    }
+
+    public Usuario getUsuarioLogado() {
+        return usuarioLogado;
+    }
+
+    public void setUsuarioLogado(Usuario usuarioLogado) {
+        this.usuarioLogado = usuarioLogado;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
--- src/main/java/br/com/trocaJogos/dao/UsuarioDao.java
+++ src/main/java/br/com/trocaJogos/dao/UsuarioDao.java
@@ -2,16 +2,33 @@ package br.com.trocaJogos.dao;
 
 import br.com.trocaJogos.model.Usuario;
 import br.com.trocaJogos.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
 
 /**
  * @author lucas
  */
 public class UsuarioDao extends GenericDao<Usuario> {
-    
+
     private HibernateUtil hibernateUtil = new HibernateUtil();
 
     public UsuarioDao() {
         super(Usuario.class);
+    }
+
+    public Usuario logar(String email, String senha) {
+        Session session = hibernateUtil.getSession();
+
+        List<Usuario> resultado = session.createQuery("select u from Usuario u WHERE u.email = :email AND u.senha = :senha")
+                .setParameter("senha", senha)
+                .setParameter("email", email)
+                .list();
+
+        if (resultado != null && !resultado.isEmpty()) {
+            return resultado.get(0);
+        } else {
+            return new Usuario();
+        }
     }
 
 }
